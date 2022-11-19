9f2479a7ca34b9b01541bed86df0e3d8255173ba
Mohammed Al-Laktah <mohammed@al-laktah.com>
2022-11-19 19:21:24 +0100

    added an option to add a --debug flag to the command line to enable debug logging

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+import sys
 from app import app
+from app.tests.data.download import get_data
 
 if __name__ == '__main__':
-    app.run(host='localhost', debug=True, port=8000)
+    if len(sys.argv) > 1 and str(sys.argv[1]) == '--debug':
+        get_data()
+        app.run(host='localhost', debug=True, port=8000)
+    else:
+        app.run(host='localhost', debug=False, port=8000)
