71632a9c37661b964283d6d3cea5dd3d21e7c0af
Fares Motia <faresmotia1@hotmail.fr>
2022-11-21 20:09:09 +0100

    cleaned the file and moved it to backend

--- app/backend/ltlcalls.py
+++ app/backend/ltlcalls.py
@@ -1,9 +1,6 @@
-#import pm4py
-import enum
-from numpy import choose
 import pm4py.algo.filtering.log.ltl as ltl
-from pm4py import read_xes
 from enum import Enum 
+
 class LTL_Rule(Enum):
     A_ev_B = 1
     A_ev_B_ev_C = 2
@@ -11,12 +8,10 @@ class LTL_Rule(Enum):
     A_nex_B_nex_C = 4
     four_eye = 5
     val_diff_person = 6
-log1 = read_xes("/Users/fares/github/A-customizable-interface-for-LTL-Checking/app/backend/CRUD/Hospital_log.xes")
-log2 = read_xes("/Users/fares/github/A-customizable-interface-for-LTL-Checking/app/backend/CRUD/running-example.xes")
 
-#log_filtered = ltl.ltl_checker.A_eventually_B(log, "examine casually", "pay compensation")
-#print(log_filtered)
-
+""" 
+choose_filter is a function that recieves a log file and an ltl rule and some activities and then based on what ltl rule has been chosen the log file will get filtered
+"""
 def choose_filter(file, filter_type: LTL_Rule, A, B, C, D):
 #1st rule : A eventually B
     if filter_type == LTL_Rule.A_ev_B:
@@ -37,9 +32,4 @@ def choose_filter(file, filter_type: LTL_Rule, A, B, C, D):
     elif filter_type == LTL_Rule.val_diff_person:
         filtered_log = ltl.ltl_checker.attr_value_different_persons(file,A)
     return filtered_log 
-log_test = choose_filter(log1, LTL_Rule.four_eye ,"Nursing ward","General Lab Clinical Chemistry",None, None)
-
-print(log1)
-print('$$$$$$$$$$$$$$$$$$$$$$$$')
-print(log_test)
 
