b26a2d83d4f689e7e19f6bb8a30605fe5b9d5f1a
Mohammed Al-Laktah <mohammed@al-laktah.com>
2022-11-22 14:13:40 +0100

    moved the spinner out of run

--- tests/data/download.py
+++ tests/data/download.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, threading, time
 from os.path import exists
 
 """
@@ -11,6 +11,20 @@ with open('tests/data/data_sources.json') as json_file:
 This function is used to download test data from web sources and saves it in the app/tests/data folder.
 """
 def get_data():
+    thread = threading.Thread(target=_get_data)
+    thread.start()
+
+    eli_count = 0
+    spinner = ['|', '/', '-', '\\']
+        
+    while thread.is_alive():
+        print(f"{spinner[eli_count]} Loading", end='\r')
+        eli_count = (eli_count + 1) % 4
+        time.sleep(.2)
+    thread.join()
+    print('Done      ')
+
+def _get_data():
     for data_url in data_urls:
         if exists('app/tests/data/' + data_url.split('/')[-1]):
             continue
