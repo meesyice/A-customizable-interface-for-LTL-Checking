9effba9af065d5162699d45c567cfaac99f877e4
Ping Wan <ping.wan@rwth-aachen.de>
2022-11-22 13:54:16 +0100

    add a function to store the imported file

--- app/frontend/templates/views/views.py
+++ app/frontend/templates/views/views.py
@@ -1,7 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['datei']
+    print(file)
+    file.save(f'app/import_files/{file.filename}')
+    return redirect('/index')
