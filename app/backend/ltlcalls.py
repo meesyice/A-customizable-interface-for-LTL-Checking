236660a6948f2923da6d5cda99011d6fc953a343
Mohammed Al-Laktah <mohammed@al-laktah.com>
2022-11-24 18:13:54 +0100

    updated apply filter to use a list

--- app/backend/ltlcalls.py
+++ app/backend/ltlcalls.py
@@ -9,30 +9,29 @@ class LTL_Rule(Enum):
     four_eye = 5
     val_diff_person = 6
 """ 
-choose_filter is a function that recieves a log file and an ltl rule and some activities and then based on what ltl rule has been chosen the log file will get filtered
+apply_filter is a function that recieves a log file and an ltl rule and some activities and then based on what ltl rule has been chosen the log file will get filtered
 """
-filtered_log = None
-def choose_filter(file, filter_type: LTL_Rule,*args):
+def apply_filter(file, filter_type: LTL_Rule, events):
                   
-    match filter_type :
+    match filter_type:
        #1st rule : A eventually B
         case LTL_Rule.A_ev_B:
-            filtered_log = ltl.ltl_checker.A_eventually_B(file, *args)
+            filtered_log = ltl.ltl_checker.A_eventually_B(file, events[0], events[1])
        #2nd rule : A eventually B evenetually C 
         case LTL_Rule.A_ev_B_ev_C:
-            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C(file, *args)
+            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C(file, events[0], events[1], events[2])
         #3rd rule : A eveventually B eventually C eventually D
         case LTL_Rule.A_ev_B_ev_C_ev_D:
-            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C_eventually_D(file,*args)
+            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C_eventually_D(file, events[0], events[1], events[2], events[3])
         #4th rule : A next to B next to C 
         case  LTL_Rule.A_nex_B_nex_C:
-            filtered_log = ltl.ltl_checker.A_next_B_next_C(file,*args)
+            filtered_log = ltl.ltl_checker.A_next_B_next_C(file, events[0], events[1], events[2])
         #5th rule : four eyes principle
         case  LTL_Rule.four_eye:
-            filtered_log = ltl.ltl_checker.four_eyes_principle(file,*args) 
+            filtered_log = ltl.ltl_checker.four_eyes_principle(file, events[0], events[1])
         #6th rule : value different persons
         case  LTL_Rule.val_diff_person:
-            filtered_log = ltl.ltl_checker.attr_value_different_persons(file,*args)
+            filtered_log = ltl.ltl_checker.attr_value_different_persons(file, events[0])
         case _ :
             filtered_log = None
 
