72b501db7ffdd106b29360f7c47a4a8d0e043897
Mohammed Al-Laktah <mohammed@al-laktah.com>
2022-11-24 19:45:29 +0100

    added a check for imported files dir

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__, template_folder='./frontend/templates', static_folder='./frontend/static')
-app.config['UPLOAD_DIRECTORY'] = 'app/import_files'
+
+if not os.path.exists('app/imported_files/'):
+    os.makedirs('app/imported_files/')
+app.config['UPLOAD_DIRECTORY'] = 'app/imported_files'
 app.config['ALLOWED_FILE_TYPE'] = ['.xes']
 app.secret_key = 'flash_key'
 
