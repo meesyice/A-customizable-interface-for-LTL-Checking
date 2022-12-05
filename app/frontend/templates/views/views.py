4ebaf1d1c3f884fc084b3e1e3c3c9f6dcbbd410d
Ping Wan <ping.wan@rwth-aachen.de>
2022-12-05 15:22:08 +0100

    define the function of saving uploaded file

--- app/frontend/templates/views/views.py
+++ app/frontend/templates/views/views.py
@@ -12,6 +12,18 @@ def index():
     files = os.listdir(app.config['UPLOAD_DIRECTORY'])
     return render_template('index.html', files = files)
 
+
+def saveFile(file):
+    file_type = os.path.splitext(file.filename)[1]
+    if file_type.lower() not in app.config['ALLOWED_FILE_TYPE']:
+        flash("Please upload a XES file")
+        return ""
+    else:
+        file_path = os.path.join(app.config['UPLOAD_DIRECTORY'], secure_filename('result.xes'))
+        file.save(file_path)
+        return file_path
+    
+
 """
 Takes a file uploaded by the user and applies the filter chosen by the user to it using arguments provided by the user.
 """
@@ -21,16 +33,13 @@ def upload():
     ltl_rule = request.form['LTL_rule']
     events = request.form.getlist('activity')
  
-    file_type = os.path.splitext(file.filename)[1]
-    if file_type.lower() not in app.config['ALLOWED_FILE_TYPE']:
-        flash("Please upload a XES file")
-        return redirect("/")
+    file_path = saveFile(file)
+    
+    if not file_path:
+        return redirect('/')
     else:
-        file_path = os.path.join(app.config['UPLOAD_DIRECTORY'], secure_filename('result.xes'))
-        file.save(file_path)
-     
-    filterd_log = apply_filter(read_xes(file_path), choose_filter(ltl_rule), events)
-    write_xes(filterd_log, file_path)
+        filterd_log = apply_filter(read_xes(file_path), choose_filter(ltl_rule), events)
+        write_xes(filterd_log, file_path)
     @after_this_request
     def delete(response):
         os.remove(os.path.join(app.config['UPLOAD_DIRECTORY'], 'result.xes'))
