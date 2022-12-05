81954ad04a22eac35296980776df703f7af1ad62
Fares Motia <faresmotia1@hotmail.fr>
2022-12-05 14:44:14 +0100

    OR function

--- app/backend/ltlcalls.py
+++ app/backend/ltlcalls.py
@@ -1,6 +1,11 @@
+from asyncore import write
 import pm4py.algo.filtering.log.ltl as ltl
-from enum import Enum 
-from pm4py import read_xes
+from enum import Enum
+from pm4py import read_xes, convert_to_dataframe, write_xes
+import pandas as pd
+import os
+
+
 class LTL_Rule(Enum):
     A_ev_B = 1
     A_ev_B_ev_C = 2
@@ -8,33 +13,43 @@ class LTL_Rule(Enum):
     A_nex_B_nex_C = 4
     four_eye = 5
     val_diff_person = 6
+
+
 """ 
-apply_filter is a function that recieves a log file and an ltl rule and some activities and then based on what ltl rule has been chosen the log file will get filtered
+apply_filter is a function that recieves a log file and an ltl rule and some activities and 
+then based on what ltl rule has been chosen the log file will get filtered
 """
 def apply_filter(file, filter_type: LTL_Rule, events):
     match filter_type:
-       #1st rule : A eventually B
+       # 1st rule : A eventually B
         case LTL_Rule.A_ev_B:
-            filtered_log = ltl.ltl_checker.A_eventually_B(file, events[0], events[1])
-       #2nd rule : A eventually B evenetually C 
+            filtered_log = ltl.ltl_checker.A_eventually_B(
+                file, events[0], events[1])
+       # 2nd rule : A eventually B evenetually C
         case LTL_Rule.A_ev_B_ev_C:
-            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C(file, events[0], events[1], events[2])
-        #3rd rule : A eveventually B eventually C eventually D
+            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C(
+                file, events[0], events[1], events[2])
+        # 3rd rule : A eveventually B eventually C eventually D
         case LTL_Rule.A_ev_B_ev_C_ev_D:
-            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C_eventually_D(file, events[0], events[1], events[2], events[3])
-        #4th rule : A next to B next to C 
+            filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C_eventually_D(
+                file, events[0], events[1], events[2], events[3])
+        # 4th rule : A next to B next to C
         case  LTL_Rule.A_nex_B_nex_C:
-            filtered_log = ltl.ltl_checker.A_next_B_next_C(file, events[0], events[1], events[2])
-        #5th rule : four eyes principle
+            filtered_log = ltl.ltl_checker.A_next_B_next_C(
+                file, events[0], events[1], events[2])
+        # 5th rule : four eyes principle
         case  LTL_Rule.four_eye:
-            filtered_log = ltl.ltl_checker.four_eyes_principle(file, events[0], events[1])
-        #6th rule : value different persons
+            filtered_log = ltl.ltl_checker.four_eyes_principle(
+                file, events[0], events[1])
+        # 6th rule : value different persons
         case  LTL_Rule.val_diff_person:
-            filtered_log = ltl.ltl_checker.attr_value_different_persons(file, events[0])
-        case _ :
+            filtered_log = ltl.ltl_checker.attr_value_different_persons(
+                file, events[0])
+        case _:
             filtered_log = None
 
-    return filtered_log 
+    return filtered_log
+
 
 def choose_filter(filter_name):
     match filter_name:
@@ -50,3 +65,22 @@ def choose_filter(filter_name):
             return LTL_Rule.four_eye
         case 'Value different persons':
             return LTL_Rule.val_diff_person
+
+""" 
+OR is a function that combine filtered logs by keeping the events that satisfy at least one filter. 
+Events are represented as list of lists where list i correspond to filter i .
+Converting our log file to data frames allows us to use some useful preimplemented panda functions like drop_duplicates().
+We return the concatenation of the data frames after the filtering happened and we drop any duplicate .
+"""
+def OR(file, filters, events):
+    filtered_files = []
+    i = 0
+    for i in range(0, len(filters)):
+        filtered_files.append(apply_filter(file, filters[i], events[i]))
+
+    dataframes = []
+    for f in filtered_files:
+        frame = convert_to_dataframe(f)
+        dataframes.append(frame)
+    
+    return pd.concat(dataframes).drop_duplicates()
