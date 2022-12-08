8a6cf7569c743b8823ba3f93311fecc0ef17bdbe
Ping Wan <ping.wan@rwth-aachen.de>
2022-12-08 10:04:47 +0100

    test for uploading

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,3 +8,17 @@ def test_homepage():
     url = '/'
     response = client.get(url)
     assert response.status_code == 200
+    
+def test_upload():
+    client = app.test_client()
+    file_name = 'running-example.xes'
+    datei = {
+        'datei': (open('tests/data/running-example.xes','rb'), file_name),
+        'LTL_rule_1':'A eventually B',
+        'activitiesOfThefirstRule':['decide', 'check ticket'],
+        'andOr':'none'
+    }
+    response = client.post('/upload', data=datei, content_type='multipart/form-data')
+    assert response.status_code == 302
+    
+    
