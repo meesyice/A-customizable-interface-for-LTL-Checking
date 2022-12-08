792e32bcd9598bb1deedaf95838279ecbf97c621
Ping Wan <ping.wan@rwth-aachen.de>
2022-12-08 12:30:14 +0100

    use fixture decorator in pytest

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,31 +1,39 @@
 import sys
+import os
+import pytest
 
 sys.path.append('.')
 from app import app
 
-def test_homepage():
-    client = app.test_client()
-    url = '/'
-    response = client.get(url)
-    assert response.status_code == 200
+@pytest.fixture(scope='module')
+def client():
+    return app.test_client()
+
+class TestRoutes:
+
+    @pytest.mark.run(order=1)
+    def test_homepage(self, client):
+        url = '/'
+        response = client.get(url)
+        assert response.status_code == 200
     
-def test_upload():
-    client = app.test_client()
-    file = open('tests/data/running-example.xes','rb')
-    datei = {
-        'datei': (file, 'running-example.xes'),
-        'LTL_rule_1':'A eventually B',
-        'activitiesOfThefirstRule':['decide', 'check ticket'],
-        'andOr':'none'
-    }
-    response = client.post('/upload', data=datei, content_type='multipart/form-data')
-    file.close()
-    assert os.path.exists(app.config['UPLOAD_DIRECTORY'] + '/result.xes')
-    assert response.status_code == 307
+    @pytest.mark.run(order=2)
+    def test_upload(self, client):
+        file = open('tests/data/running-example.xes','rb')
+        datei = {
+            'datei': (file, 'running-example.xes'),
+            'LTL_rule_1':'A eventually B',
+            'activitiesOfThefirstRule':['decide', 'check ticket'],
+            'andOr':'none'
+        }
+        response = client.post('/upload', data=datei, content_type='multipart/form-data')
+        file.close()
+        assert os.path.exists(app.config['UPLOAD_DIRECTORY'] + '/result.xes')
+        assert response.status_code == 307
     
-def test_delete():
-    client = app.test_client()
-    response = client.post('/result')
-    assert not os.path.exists(app.config['UPLOAD_DIRECTORY'] + '/result.xes')
-    assert response.status_code == 200
+    @pytest.mark.run(order=3)
+    def test_delete(self, client):
+        response = client.post('/result')
+        assert not os.path.exists(app.config['UPLOAD_DIRECTORY'] + '/result.xes')
+        assert response.status_code == 200
     
