10106abd452b60c8610a974d75d08dc5253dd3de
Ping Wan <ping.wan@rwth-aachen.de>
2022-12-16 16:42:28 +0100

    fix a bug

--- app/backend/CRUD/update.py
+++ app/backend/CRUD/update.py
@@ -9,13 +9,14 @@ Apply LTL rules to process the file and overwrite the original file with the pro
 def writeFile(file_path):
     # get LTL Rules and then we need parse them
     rules, activities = readLTLRulesAndActivities()
-    json_file = saveAsJSON(rules, activities)
+    json_file = saveAsJSON(activities)
     print(json_file)
     
 
-def saveAsJSON(rules:str, activities:list):
-    rules = re.split('LTL_And|LTL_Or', rules.replace('(','').replace(')','').replace(' ',''))
+def saveAsJSON(activities:list):
+    # rules = re.split('LTL_And|LTL_Or', rules.replace('(','').replace(')','').replace(' ',''))
     res = dict()
     for index in range(len(activities)):
-        res[rules[index]] = activities[index]
+        # res[rules[index]] = activities[index]
+        res[str(index)] = activities[index]
     return json.dumps(res)
