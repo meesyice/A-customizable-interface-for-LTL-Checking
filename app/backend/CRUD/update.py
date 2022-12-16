c22847cf23823e22e947b5e9702340695c3916c8
Ping Wan <ping.wan@rwth-aachen.de>
2022-12-16 18:03:27 +0100

    delete unuseful function

--- app/backend/CRUD/update.py
+++ app/backend/CRUD/update.py
@@ -9,16 +9,4 @@ Apply LTL rules to process the file and overwrite the original file with the pro
 def writeFile(file_path):
     # get LTL Rules and then we need parse them
     rules, activities = readLTLRulesAndActivities()
-    json_file = saveAsJSON(activities)
-    print(json_file)
     
-"""
-transfer activities to a json file
-"""
-def saveAsJSON(activities:list):
-    # rules = re.split('LTL_And|LTL_Or', rules.replace('(','').replace(')','').replace(' ',''))
-    res = dict()
-    for index in range(len(activities)):
-        # res[rules[index]] = activities[index]
-        res[str(index)] = activities[index]
-    return json.dumps(res)
