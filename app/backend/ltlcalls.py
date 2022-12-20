ad9d36fcac9dcc3c1375bed419124588ff5087a5
Mohammed Al-Laktah <mohammed@al-laktah.com>
2022-12-20 04:42:38 +0100

    updated and & or, added brackets, updated parser

--- app/backend/ltlcalls.py
+++ app/backend/ltlcalls.py
@@ -18,12 +18,16 @@ class LTL_Combiner(Enum):
     And = 1
     Or = 2
 
+class LTL_Bracket(Enum):
+    Left = 1
+    Right = 2
+
 
 """ 
 apply_filter is a function that recieves a log file and an ltl rule and some activities and 
 then based on what ltl rule has been chosen the log file will get filtered
 """
-def apply_filter(file, filter_type: LTL_Rule, events):
+def apply_filter(file, filter_type: LTL_Rule, events: list[str]):
     match filter_type:
        # 1st rule : A eventually B
         case LTL_Rule.A_ev_B:
@@ -82,19 +86,27 @@ def choose_combiner(combiner_name):
             return None
             
 
+def choose_Bracket(bracket_name):
+    match bracket_name:
+        case 'LTL_LB':
+            return LTL_Bracket.Left
+        case 'LTL_RB':
+            return LTL_Bracket.Right
+        case _:
+            return None
+
 """ 
 OR is a function that combine filtered logs by keeping the events that satisfy at least one filter. 
 Events are represented as list of lists where list i correspond to filter i .
 Converting our log file to data frames allows us to use some useful preimplemented panda functions like drop_duplicates().
 We return the concatenation of the data frames after the filtering happened and we drop any duplicates.
 """
-def OR(file, filters: LTL_Rule, events: list[list[str]]):
-    dataframes = []
-    i = 0 
-    for filter in filters:
-        dataframes.append(convert_to_dataframe(apply_filter(file, filter, events[i])))
-        i+=1
-    if len(dataframes[0]) == 0:
+def OR(file, filter: LTL_Rule, events: list[str]):
+    dataframes = [convert_to_dataframe(file), convert_to_dataframe(apply_filter(file, filter, events))]
+    
+    if len(dataframes[0]) == 0 and len(dataframes[1]) == 0:
+        return None
+    elif len(dataframes[0]) == 0:
         return dataframes[1]
     elif len(dataframes[1]) == 0:
         return dataframes[0]
@@ -107,27 +119,23 @@ Events are represented as list of lists where list i correspond to filter i .
 Converting our log file to data frames allows us to use some useful preimplemented panda functions like drop_duplicates().
 We return the intersection of the data frames after the filtering happened and we drop any duplicates.
 """
-def AND(file, filters: LTL_Rule, events: list[list[str]]):
-    dataframes = []
-    i = 0
-    for filter in filters:
-        dataframes.append(convert_to_dataframe(apply_filter(file, filter, events[i])))
-        i+=1
-    if len(dataframes[0]) == 0 or len(dataframes[1]) == 0:
-        return pd.DataFrame()
-    else:
-        return pd.merge(dataframes[0],dataframes[1], how='inner').drop_duplicates()
+def AND(file, filter: LTL_Rule, events: list[str]):
+    apply_filter(file, filter, events)
 
 
-def parse_mod_ltl(mod_LTL_Rule: str):
-    filters = []
-    combiners = []
-    for subrule in mod_LTL_Rule.split(" "):
-        print(subrule)
-        if subrule in ["LTL_And", "LTL_Or"]:
-            combiners.append(choose_combiner(subrule))
+
+def parse_mod_ltl(str_LTL_Rule: str):
+    parsed_LTL_Rule = []
+    for subrule in str_LTL_Rule.split(" "):
+        if subrule in ["LTL_LB", "LTL_RB"]:
+            parsed_LTL_Rule.append(choose_Bracket(subrule))
+        elif subrule in ["LTL_And", "LTL_Or"]:
+            parsed_LTL_Rule.append(choose_combiner(subrule))
+        elif subrule in ["LTL_A_ev_B", "LTL_A_ev_B_ev_C", "LTL_A_ev_B_ev_C_ev_D", "LTL_A_nex_B_nex_C", "LTL_Four_Eyes_Principle", "LTL_Attr_Val_Diff_Persons"]:
+            parsed_LTL_Rule.append(choose_filter(subrule))
         else:
-            filters.append(choose_filter(subrule))
-    return filters, combiners
+            continue
+    return parsed_LTL_Rule
 
-print(parse_mod_ltl("LTL_A_ev_B LTL_Or LTL_A_ev_B_ev_C LTL_And LTL_A_ev_B_ev_C_ev_D"))
+def apply_rule(file, str_LTL_Rule: str, events: list[list[str]]):
+    pass
