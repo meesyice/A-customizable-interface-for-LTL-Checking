f8071b248ecdd5862be42f43c8daff2606f8d1cf
Ping Wan <ping.wan@rwth-aachen.de>
2023-01-07 21:25:28 +0100

    rewrite function

--- app/backend/CRUD/update.py
+++ app/backend/CRUD/update.py
@@ -1,12 +1,19 @@
-import re, json
+import os
 from pm4py import write_xes, read_xes
-
+from app import app
+from werkzeug.utils import secure_filename
 from app.backend.CRUD.read import readLTLRulesAndActivities
+from app.backend.ltlcalls import Conversion, apply_rule
 
 """
 Apply LTL rules to process the file and overwrite the original file with the processed file
 """    
 def writeFile(file_path):
     # get LTL Rules and then we need parse them
-    rules, activities = readLTLRulesAndActivities()
-    print(rules, '\n', activities)
+    expr, activities = readLTLRulesAndActivities()
+    print(expr, activities)
+    expr = Conversion(len(expr)).infixToPostfix(expr).replace('-',' LTL_And ').replace('+', ' LTL_Or ')
+    result_path = os.path.join(app.config['UPLOAD_DIRECTORY'], secure_filename('result.xes'))
+    input_log = read_xes(file_path)
+    filtered_log = apply_rule(input_log, expr.split(), activities)
+    write_xes(filtered_log, result_path)
